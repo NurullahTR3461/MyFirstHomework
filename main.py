58b562e2453a8d3e052a79c85164fff00a52f5e2
Nurullah Alçep <nurullahalcep@gmail.com>
2024-07-29 16:03:30 +0300

    Homework is finished by Nurullah

---
+++ main.py
@@ -0,0 +1,5 @@
+from plyer import notification
+
+title = "homework"
+message = "to protect innocent people all around the world for the people's future"
+notification.notify(title = title, message=message)
